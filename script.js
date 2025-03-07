58fe903919f3782ccab2f2ca02d969805c5e1294
JOSHUA <oshiwaplay@gmail.com>
2025-03-07 15:13:41 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", () => {
+  let hero = document.querySelector("#hero");
+  hero.classList.add("show");
+});
+
+let year = document.getElementById("year");
+year.innerText = new Date().getFullYear();
+
+let navigation = document.querySelector(".navigation");
+let navigationContent = document.createElement("section");
+
+navigationContent.innerHTML = `
+      <header>
+        <a href="index.html">
+          <img src="images/ucu-logo.png" alt="UCU Logo" />
+        </a>
+
+        <section class="desktop">
+          <a href="index.html">Home</a>
+          <a href="ucu-smart-eco-campus.html">UCU Smart Eco Campus</a>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+        </section>
+
+        <section class="mobile">
+          <button>menu</button>
+        </section>
+      </header>
+
+      <section class="links">
+        <a href="index.html">Home</a>
+        <a href="ucu-smart-eco-campus.html">UCU Smart Eco Campus</a>
+        <a href="#">Projects</a>
+        <a href="#">About</a>
+      </section>
+`;
+
+navigation.appendChild(navigationContent);
+
+let showMenu = document.querySelector(".mobile button");
+let menuLinks = document.querySelector(".links");
+
+showMenu.onclick = () => {
+  if (menuLinks.style.display === "grid") {
+    menuLinks.style.display = "none";
+  } else {
+    menuLinks.style.display = "grid";
+  }
+};
---
+++ ucu-sec-script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  let hero = document.querySelector("#hero");
+  let uiImg = document.querySelector(".ui-img");
+  hero.classList.add("show");
+  uiImg.classList.add("show");
+});
